821ec2cbcd1aed6e6dee83b59d565a7ae098cc7b
itm <itmagistr@gmail.com>
2021-03-10 01:13:36 +0300

    изменен результат выдачи списка языковых констант

--- vh_dict/serializers.py
+++ vh_dict/serializers.py
@@ -10,6 +10,9 @@ class DictStrRuSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = DictString
 		fields = ['code', 'title', 'title_short', ]
+	def to_representation(self, data):
+		res = super(DictStrRuSerializer, self).to_representation(data)
+		return {res['code']: res}
 
 class DictStrEnSerializer(serializers.ModelSerializer):
 	title = serializers.CharField(source='title_en')
@@ -17,10 +20,13 @@ class DictStrEnSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = DictString
 		fields = ['code', 'title', 'title_short', ]
+	def to_representation(self, data):
+		res = super(DictStrEnSerializer, self).to_representation(data)
+		return {res['code']: res}
 
 class DictStrFilterSerializer(serializers.Serializer):
 	code = serializers.CharField(max_length=20, allow_blank=False)
-                	
+					
 	def get_fields(self):
 		new_fields = collections.OrderedDict()
 		for name, field in super().get_fields().items():
