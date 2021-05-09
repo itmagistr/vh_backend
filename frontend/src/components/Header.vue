779af654f850fba76e8e5ec3e33efef520905fa6
W11N4R7 <pohorielov.m@gmail.com>
2021-05-09 19:01:51 +0100

    Minor update CSS (Header highlight + Integrate flex)

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,28 +2,40 @@
   <header>
     <nav v-if="resize === false" class="navbar navbar-expand-xl navbar-light">
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav hract">
           <li class="nav-item" :id="[$route.name === 'service' ? 'active' : '']">
+            <hr>
             <router-link class="nav-link" :to="{name: 'service'}">{{ $t('menu.services') }}</router-link>
+            <hr>
           </li>
           <li class="nav-item" :id="[$route.name === 'booking' ? 'active' : '']">
+            <hr>
             <router-link class="nav-link active" :to="{name: 'booking'}">{{ $t('menu.booking') }}</router-link>
+            <hr>
           </li>
           <li class="nav-item" :id="[$route.name === 'doctors' ? 'active' : '']">
+            <hr>
             <router-link class="nav-link" :to="{name: 'doctors'}">{{ $t('menu.doctors') }}</router-link>
+            <hr>
           </li>
           <li class="nav-item">
+            <hr>
             <a href="#" class="nav-link" data-toggle="modal" data-target="#mdl-future-ok" data-name="тур">{{ $t('menu.virtual_tour') }}</a>
+            <hr>
           </li>
           <li class="nav-item">
+            <hr>
             <a href="#" class="nav-link" data-toggle="modal" data-target="#mdl-future-ok" data-name="докум">{{ $t('menu.documentation') }}</a>
+            <hr>
           </li>
           <li class="nav-item">
+            <hr>
             <a href="#" class="nav-link" data-toggle="modal" data-target="#mdl-contacts">{{ $t('menu.contacts') }}</a>
+            <hr>
           </li>
         </ul>
-        <ul class="navbar-nav mr-auto"><router-link :to="{name: 'main'}"><img class="logo" src="/img/logo-sm.svg"/></router-link></ul>
-        <ul class="navbar-nav">
+        <ul class="navbar-nav"><router-link :to="{name: 'main'}"><img class="logo" src="/img/logo-sm.svg"/></router-link></ul>
+        <ul class="navbar-nav rightM">
           <li class="nav-item">
             <div class="inner-addon right-addon">
               <i class="fas fa-search" aria-hidden="true" data-toggle="modal" data-target="#mdl-future-ok"></i>
@@ -38,13 +50,9 @@
               <a class="dropdown-item" @click="chLang('en')">English</a>
             </div>
           </li>
-        </ul>
-        <ul class="navbar-nav">
           <li class="nav-item">
             <router-link class="nav-link" to="#"><h6 id="profile_field">{{ $t('menu.profile')}} </h6> <i class="fas fa-user"></i></router-link>
           </li>
-        </ul>
-        <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link number" href="https://api.whatsapp.com/send?phone=74950238669" target="_blank">+7 495 023 86 69</a>
             <a class="nav-link" href="#" id="order-call" data-toggle="modal" data-target="#mdl-call-back">{{ $t('menu.callback_lnk')}}</a>
@@ -56,12 +64,8 @@
       <button class="navbar-toggler" type="button" data-toggle="modal" data-target="#mdlm-menu">
         <i class="fas fa-bars"></i>
       </button>
-      <ul class="navbar-nav"><router-link :to="{name: 'main'}"><img class="mobile logo" src="/img/logo-sm.svg"/></router-link></ul>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-            <i class="fas fa-search" data-toggle="modal" data-target="#mdl-future-ok"></i>
-        </li>
-      </ul>
+      <router-link :to="{name: 'main'}"><img class="mobile logo" src="/img/logo-sm.svg"/></router-link>
+      <i class="fas fa-search" data-toggle="modal" data-target="#mdl-future-ok"></i>
     </nav>
   </header>
 </template>
@@ -87,6 +91,7 @@ export default {
 <style lang="sass">
 @import "@/styles/_variables.sass"
 
+$heightHeader: 136px
 
 .dropdown-submenu
   position: relative
@@ -101,28 +106,6 @@ export default {
     margin-left: .1rem
     margin-right: .1rem
 
-header
-  background: rgba(254, 253, 251, 0.64)
-  backdrop-filter: blur(32px)
-  #nDML
-    font-size: 1rem
-    text-transform: uppercase
-    color: $button-color!important
-  h6#profile_field
-    line-height: 21px
-    margin-right: 8px
-    display: inline
-.logo
-  width: 250px
-
-#order-call
-  text-align: right
-  padding-top: .25rem
-  color: $active-text-link!important
-
-#active > a
-  color: $active-text-link!important
-
 .navbar-light a, .navbar-light, .bg-light, .bg-light a
   font-family: FuturaBookC
   font-style: normal
@@ -132,26 +115,70 @@ header
   letter-spacing: 0.05em
   color: $header_text!important
 
-
-.mr-auto a
-  text-transform: uppercase
-
-nav
-  max-width: 1560px
-  margin: auto
-  height: 136px
-  .toggler
-    display: none
-    width: 100%
-.btn-outline
-  color: $header_text
-  background: #FFF
+header
+  background: rgba(254, 253, 251, 0.64)
+  backdrop-filter: blur(32px)
+  font-family: FuturaBookC
+  > nav.navbar
+    max-width: 1560px
+    margin: auto
+    height: $heightHeader
+    > div
+      justify-content: space-between
+      > ul
+        > a
+          > img.logo
+            width: 250px
+        > li
+          > a
+            &#nDML
+              font-size: 1rem
+              text-transform: uppercase
+              color: $button-color!important
+            &#order-call
+              text-align: right
+              padding-top: .25rem
+              color: $active-text-link!important
+            &.number
+              font-size: 21px!important
+              line-height: 16px!important
+              text-align: right
+              padding-bottom: .25rem
+            > h6#profile_field
+              line-height: 21px
+              margin-right: 8px
+              display: inline-flex
+        &.rightM
+          align-items: center
+        &.hract
+          text-transform: uppercase
+          height: $heightHeader
+          > li
+            display: flex
+            flex-direction: column
+            justify-content: space-between
+            align-items: center
+            > hr
+              border: none
+              background: transparent
+              width: 40px
+              height: 4px
+              margin: 0
+            &#active
+              > hr
+                background: #42e1c5
+              > a
+                color: $active-text-link!important
+    .toggler
+      display: none
+      width: 100%
+  .btn-outline
+    color: $header_text
+    background: #FFF
 
 .btn-outline
   &:hover
     color: $header_text
-
-
 
 // Change standart color of input
 .form-control
@@ -185,12 +212,6 @@ nav
 .right-addon input
   padding-right: 30px
 
-.number
-  font-size: 21px!important
-  line-height: 16px!important
-  text-align: right
-  padding-bottom: .25rem
-
 .right-side
   position: absolute
   right: 7.3%
@@ -199,10 +220,9 @@ nav
 @media (max-width: 1399px)
   .navbar-light
       color: $white
-  nav
-    font-family: FuturaBookC
-    font-size: 27px
-    line-height: 33px
-    text-align: center
-    height: 64px
+  header
+    > nav.navbar
+      height: 64px
+      > i.fas.fa-search
+        transform: scaleX(-1)
 </style>
