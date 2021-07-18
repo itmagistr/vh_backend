67365db7005dc490787243d599a8476c86731216
W11N4R7 <pohorielov.m@gmail.com>
2021-07-18 21:06:35 +0100

    Added Virtual Tour page file

---
+++ frontend/src/pages/VirtualTour.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="wrapper">
+    <div class="proto2">
+      <div v-for="image in images" :key="image.src">
+        <img :src="image.src" @click="imgShow(image.src)" data-toggle="modal" data-target="#mdl-image">
+      </div>
+    </div>
+     <div class="proto2">
+      <div v-for="video in videos" :key="video.src">
+        <video @click="videoShow(video.src)" data-toggle="modal" data-target="#mdl-image">
+          <source :src="video.src" type="video/mp4"/>
+        </video>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['resize'],
+  data() {
+    return {
+      images: [
+        {src: `https://vh.v-hollywood.ru/media/virttur/hollywood_dentistry_gal2.jpg`},
+        {src: `https://vh.v-hollywood.ru/media/virttur/hollywood_dentistry_gal4.jpg`},
+        {src: `https://vh.v-hollywood.ru/media/virttur/image1.jpeg`},
+        {src: `https://vh.v-hollywood.ru/media/virttur/image0.jpeg`},
+        {src: `https://vh.v-hollywood.ru/media/virttur/image2.jpeg`}],
+      videos: [
+        {src: `https://vh.v-hollywood.ru/media/virttur/181336916.mp4`},
+        {src: `https://vh.v-hollywood.ru/media/virttur/93671.mp4`},
+        {src: `https://vh.v-hollywood.ru/media/virttur/93672.mp4`}
+      ],
+    }
+  },
+  methods: {
+    imgShow(val){
+      this.$root.$emit("imgShowGL", val, false);
+    },
+    videoShow(val){
+      this.$root.$emit("imgShowGL", val, true);
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import "@/styles/_variables.sass"
+
+body.tour
+  background: #F6F3ED
+  > header
+    background: #F6F3ED
+  > main
+    padding: 27px 0 2rem
+  .wrapper
+    width: calc(100% - 300px)
+    .proto2
+      padding: 1rem
+      height: 320px
+      width: 100%
+      overflow: auto
+      background: #F3E9D4
+      border-radius: 1rem
+      margin: 27px 0
+      display: flex
+      > div
+        margin: 0 .5rem
+        &:first-child
+          margin-left: 0
+        &:last-child
+          margin-right: 0
+        > img, video
+          height: 100%
+          border-radius: .5rem
+    .splide
+      padding: 0 0 3rem
+@media (max-width: 1399px)
+  body.tour
+    background: #F6F3ED
+    > header
+      background: #F6F3ED
+    > main
+      padding: 0
+    .wrapper
+      margin: auto
+      width: calc(100% - 2rem)
+</style>
