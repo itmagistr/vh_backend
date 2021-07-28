0cac660e4c7d5318a36d24ae37f09bf1fe0f573f
itmagistr <itmagistr@gmail.com>
2021-07-28 23:35:53 +0300

    update db table product
    extends title_check field length to 100

--- vh_product/models.py
+++ vh_product/models.py
@@ -8,7 +8,7 @@ class Product(PolymorphicModel):
 	uid = models.UUIDField(default=uuid.uuid4, editable=False)
 	code = models.CharField(max_length=20, null=False, blank=False)
 	title = models.CharField(max_length=255, null=False, blank=False)
-	title_check = models.CharField(max_length=55, null=False, blank=False)
+	title_check = models.CharField(max_length=100, null=False, blank=False)
 	description = models.TextField(null=True, blank=True)
 	price = models.DecimalField(max_digits=10, default=1000, decimal_places=2)
 	price_old = models.DecimalField(max_digits=10, default=1000, decimal_places=2)
